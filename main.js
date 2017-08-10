d7d2d53dda28872600eeaf92a8bc5910c42a7036
jchaasis <jchaasis@gmail.com>
2017-08-09 20:29:09 -0400

    operations are fully functional with any length operand.

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
     //arrays from the html buttons
 let button = document.querySelectorAll("button");
 let nums = document.querySelectorAll(".numberButton");
-let ops = document.querySelectorAll(".operator");
 
     // variables from htmnl elements
 let body = document.querySelector("#body");
@@ -13,38 +12,31 @@ let clear = document.querySelector('#clear');
 
 
 //if we click a number button, we want the value of that button to display in the display box
-// let displayStr='';
 let operand1='';
 let operand2='';
 let operator='';
 
 let displayNumber= function(num)  {
-
-            display.innerHTML = `<p>${operand1+operator+operand2}</p>`;
+          display.innerHTML = `<p>${operand1+operator+operand2}</p>`;
           }
 
 // if we click a button, it will pull the button value from the button array.
+
 for (let b=0; b<button.length; b++){
     button[b].addEventListener('click', function () {
+
   //create operand1
-  // if (operator === ''){
-  //   operand1 += button[b].value;
-  // }
-  if (operand1 === ''){
-    operand1 = Number(button[b].value, 10);
+  if ((operator === '') && (button[b].value !== "+" && button[b].value !== "-" && button[b].value !== "*" && button[b].value !== "/")){
+    operand1 += (button[b].value);
   }
+
   //create operator
   else if (button[b].value === "+" || button[b].value === "-" || button[b].value === "*" || button[b].value === "/"){
-    operator = button[b].value;
-  }
+    operator = button[b].value;}
+
   //create operand2
-
-  // else if (operator === "+" || operator === "-" ||operator === "*" ||operator === "/"){
-  //   operand2 += button[b].value;}
-  // });
-
-  else if (operand2 === ''){
-    operand2 = Number(button[b].value, 10);
+  else if ((operator === '+' || operator === '-' || operator === '*' || operator === '/') && (button[b].value !== "=")){
+    operand2 += (button[b].value);
   }
     displayNumber(button[b].value);
     });
@@ -58,8 +50,8 @@ equals.addEventListener("click", function(){
 
 // perform the designated operation
   let perform = null;
-
-
+  parseFloat(operand1);
+  parseFloat(operand2);
   if (operator === '+') {
     perform = operand1 + operand2;
   } else if (operator === '-') {
@@ -71,14 +63,12 @@ equals.addEventListener("click", function(){
   }
 
   display.innerHTML=`${perform}`;
-  console.log(perform);
+  
+
 });
 
-
-
-// if the Clear button is pressed, clear the display
+// if the Clear button is pressed, clear the display and clear the operands and operator
 clear.addEventListener("click",function(){
-  displayStr= '';
   operand1= '';
   operand2= '';
   operator= '';
